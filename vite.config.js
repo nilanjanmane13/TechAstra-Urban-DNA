70696a73454fa4ce3f8bacf1763571ca709845c7
Rupak Mahajan <rupakmahajan13@gmail.com>
2026-09-12 10:42:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   server: {
     host: '0.0.0.0',
     allowedHosts: ['techastra-urban-dna.onrender.com'],
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        login: path.resolve(__dirname, 'login.html'),
+      },
+    },
   },
 });
